3e8e4f2d9e7a6e5f20897e59763f6e7069fbd2e4
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-10-26 00:48:12 +0100

    Image Cropper

---
+++ ImageCropper/movableimage.h
@@ -0,0 +1,14 @@
+#ifndef MOVABLEIMAGE_H
+#define MOVABLEIMAGE_H
+
+#include <QLabel>
+#include <QWidget>
+
+class MovableImage : public QLabel
+{
+    Q_OBJECT
+public:
+    MovableImage(QWidget* parent);
+};
+
+#endif // MOVABLEIMAGE_H
