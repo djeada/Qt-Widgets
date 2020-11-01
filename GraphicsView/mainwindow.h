dceabd52142ede2e7a65f6f6a1817eaaf0cf9c17
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-02 00:38:43 +0100

    Example usage of qgraphicitem
     and how to make it movable

---
+++ GraphicsView/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_addButton_released();
+
+    void on_removeButton_released();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene* scene;
+
+};
+#endif // MAINWINDOW_H
---
+++ GraphicsView/movableimage.h
@@ -0,0 +1,18 @@
+#ifndef MOVABLEIMAGE_H
+#define MOVABLEIMAGE_H
+
+#include <QGraphicsItem>
+
+class MovableImage : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+public:
+    MovableImage(QObject *parent);
+
+private:
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+};
+
+#endif // MOVABLEIMAGE_H
