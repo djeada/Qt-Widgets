28c0aa1f3d9ce85918e57715c738863daa5b392a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-11-02 01:21:35 +0100

    Example usage of webcam

---
+++ webcam/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QCamera>
+#include <QMediaRecorder>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QMainWindow* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_devices_currentIndexChanged(const QString &arg1);
+    void on_connectButton_released();
+    void on_recordButton_released();
+
+private:
+    Ui::MainWindow *ui;
+    bool connected;
+    bool recording;
+    QCamera* camera;
+    QMediaRecorder* recorder;
+    void toggleConnected();
+    void toggleRecording();
+};
+#endif // MAINWINDOW_H
