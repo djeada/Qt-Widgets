976e665300732128ee9176162b1b923603be6647
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2021-01-19 08:55:00 +0100

    added example with synchronizing threads

---
+++ Concurrent computing/SynchronizingThreads/computingworker.cpp
@@ -0,0 +1,14 @@
+#include "computingworker.h"
+
+ComputingWorker::ComputingWorker(QObject *parent) : QObject(parent)
+{
+
+}
+void ComputingWorker::process(int n) {
+    int result = 0;
+    for (int i = 0; i < n; ++i)
+        result++;
+
+    emit showResults(result);
+    emit doneProcess();
+}
