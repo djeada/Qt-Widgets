4c81980c33f754409fc548dae887af5e63349e11
djeada <addjellouli1@gmail.com>
2022-05-24 23:53:43 +0200

    [new feature]: adding custom item delegate

---
+++ src/mvc/cpp/item_delegate/custom_delegate.h
@@ -0,0 +1,28 @@
+
+#ifndef CUSTOM_DELEGATE_H
+#define CUSTOM_DELEGATE_H
+
+
+#include <QStyledItemDelegate>
+
+
+class CustomDelegate : public QStyledItemDelegate
+{
+  Q_OBJECT
+
+public:
+    CustomDelegate(QObject *parent = nullptr);
+    ~CustomDelegate();
+
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
+
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
+
+private:
+    QLineEdit *editor;
+};
+
+#endif // CUSTOM_DELEGATE_H
---
+++ src/mvc/cpp/item_delegate/main_window.cpp
@@ -0,0 +1,27 @@
+#include "main_window.h"
+#include "./ui_main_window.h"
+#include "custom_delegate.h"
+#include <QModelIndex>
+#include <QStringListModel>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+
+  auto model = new QStringListModel(this);
+  model->setStringList(QStringList() << "Item 1"
+                                     << "73"
+                                     << "Item 3"
+                                      << "Item 4"
+                                      << "Item 5"
+                                      << "Item 6"
+                                      << "Item 7"
+                                      << "Item 8"
+                                      << "Item 9"
+  );
+  ui->listView->setModel(model);
+  ui->listView->setItemDelegate(new CustomDelegate(this));
+
+}
+
+MainWindow::~MainWindow() { delete ui; }
